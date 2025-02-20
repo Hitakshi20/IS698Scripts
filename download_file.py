44ecc124ecf2e83070560ced925e37bfbe6f2e37
Hitakshi Tanna <65662567+Hitakshi20@users.noreply.github.com>
2025-02-20 10:41:35 -0500

    Delete download_file.py

--- download_file.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-s3 = boto3.client('s3')
-bucket_name = 'my-boto3-s3-bucket-hitakshi'
-file_name = 'example.txt'
-download_name = 'downloaded-file.txt'
-
-s3.download_file(bucket_name, file_name, download_name)
-print(f'File {file_name} downloaded successfully as {download_name}!')
