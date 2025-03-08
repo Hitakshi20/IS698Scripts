49c27912ae6d363e432ffdc757320f70e3fa3211
Hitakshi Tanna <65662567+Hitakshi20@users.noreply.github.com>
2025-03-08 17:01:10 -0500

    Update and rename vpc copy.tf to vpc.tf

--- Lab-6/vpc.tf
+++ Lab-6/vpc.tf
@@ -1,16 +1,16 @@
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"  
+  cidr_block = "<your-cidr-block>"  
 }
 
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.1.0/24"  
+  cidr_block              = "<your-public-cidr>"  
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
+  availability_zone       = "<your-az>"
 }
 
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.2.0/24"  
-  availability_zone = "us-east-1b"
+  cidr_block        = "<your-private-cidr>"  
+  availability_zone = "<your-az>"
 }
